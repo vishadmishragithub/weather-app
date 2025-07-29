9ff0dcfa8de1c01e80aff0d0d5b8858a8ada5aed
Vishad <vishad@users-mbp.home>
2025-07-28 20:40:30 -0400

    Updating controller tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_path
+    assert_dom "h1", "Weather App"
     assert_response :success
   end
 end
--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class WeatherControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get weather_path, params: { zipcode: "test" }
+    assert_dom "h1", "Current temperature - 12 C"
     assert_response :success
   end
 end
