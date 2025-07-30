568140d29505b5ac663fa4e2179649592e68504f
Vishad <vishadmishra@gmail.com>
2025-07-30 12:05:52 -0400

    Creating an error reporting mechanism to show notices from rails controllers

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,6 +1,9 @@
 class WeatherController < ApplicationController
   def show
     zipcode = params[:zipcode]
+    if zipcode == "error"
+      redirect_to root_path, notice: "Address not found"
+    end
     @weather = { "temperature": 12 }
   end
 end
--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -3,7 +3,16 @@ require "test_helper"
 class WeatherControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get weather_path, params: { zipcode: "test" }
-    assert_dom "h1", "Current temperature - 12 C"
     assert_response :success
+  end
+
+  test "should render Current tempreture" do
+    get weather_path, params: { zipcode: "test" }
+    assert_dom "h1", "Current temperature - 12 C"
+  end
+  test "should redirect if address not found" do
+    get weather_path, params: { zipcode: "error" }
+    assert_response :redirect
+    assert_match root_path, @response.redirect_url
   end
 end
