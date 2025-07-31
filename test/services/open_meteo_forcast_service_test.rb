bc16f171b1284b92c0da23269758f4a0fbb56def
Vishad <vishadmishra@gmail.com>
2025-07-31 14:55:47 -0400

    Fixing expectations for forcast service test

--- test/services/open_meteo_forcast_service_test.rb
+++ test/services/open_meteo_forcast_service_test.rb
@@ -3,16 +3,19 @@ require "test_helper"
 class OpenMeteoForcastServiceTest < ActiveSupport::TestCase
   test "it returns the weather forcast for valid coordinate" do
     coordinate = CoordinateDto.new(1, 2)
+    date = Date.new(2025, 1, 1)
     stub_successfull_open_meteo_response(
       coordinate,
       expected_current_temp: 21,
       expected_high_temp: 25,
       expected_low_temp: 19,
-      date: Date.new(2025, 1, 1))
+      date: date)
 
     weather = OpenMeteoForcastService.new.get_forcast(coordinate)
-    assert(coordinate.lat == 1, "Wrong coordinate")
-    assert(coordinate.lon == 2, "Wrong coordinate")
+    assert(weather.current_temp == 21, "Wrong weather")
+    assert(weather.daily_high_low_temp[0].date == date, "Wrong high low forcast date")
+    assert(weather.daily_high_low_temp[0].high == 25, "Wrong high low forcast high")
+    assert(weather.daily_high_low_temp[0].low == 19, "Wrong high low forcast low")
   end
 
   test "it raises error when open meteo responds with error" do
